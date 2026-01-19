4c61e49d7855c8383467dbab17ef0b1562a1936e
Tony Nguyen <19918239+Tottitov@users.noreply.github.com>
2026-01-19 01:19:25 -0600

    hide vim prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,15 @@
+if status is-login
+    source ~/.config/shell/profile
+end
+
 if status is-interactive
     source ~/.config/shell/aliasrc
 
     fish_vi_key_bindings
 
     set -g fish_greeting
+    set -g fish_color_valid_path
+    set -g fish_complete_hidden true
     set -g fish_prompt_pwd_dir_length 1
 
     set -g hydro_symbol_prompt 
