2a607da1e00dac6a358e3ae9b76a91243646bc86
Tony Nguyen <19918239+Tottitov@users.noreply.github.com>
2026-02-07 23:13:58 -0600

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,7 @@ if status is-interactive
     set -g fish_greeting
     set -g fish_color_valid_path
     set -g fish_complete_hidden true
-    set -g fish_prompt_pwd_dir_length 1
+    set -g fish_prompt_pwd_dir_length 100
 
     set -g hydro_symbol_prompt 
     set -g hydro_multiline true
